d87a17542242038dfec0a78469918ce6940a6c6c
Jefferson Douglas <jedouglas.study@gmail.com>
2025-06-11 20:17:11 -0300

    commit inicial

--- Projeto1APOO/Program.cs
+++ Projeto1APOO/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Projeto1APOO
 {
@@ -10,6 +6,41 @@ namespace Projeto1APOO
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("### Etapa 1: Usando o Builder para criar objetos Tema complexos ###");
+            
+            Tema temaEscuro = new Tema.TemaBuilder()
+                .ComCorDeFundo("#2d2d2d")
+                .ComCorDaFonte("#e0e0e0")
+                .ComFonte("Consolas", 14)
+                .Build();
+
+            Tema temaClaro = new Tema.TemaBuilder()
+                .ComCorDeFundo("#fafafa")
+                .ComCorDaFonte("#333333")
+                .ComFonte("Arial", 12)
+                .Build();
+
+            Console.WriteLine("Temas criados com sucesso.\n");
+
+            Console.WriteLine("### Etapa 2: Configurando o Gerenciador (Singleton) e usando a Fábrica (Factory Method) ###");
+            
+            GerenciadorDeTemas gerenciador = GerenciadorWindows.GetInstance();
+
+            // ---- Teste com Tema Escuro ----
+            Console.WriteLine("\n--- Usando o Tema Escuro ---");
+            gerenciador.SetTemaAtivo(temaEscuro);
+
+            Botao botaoEscuro = gerenciador.CriarBotao();
+            Console.Write("--> Resultado: ");
+            botaoEscuro.Renderizar();
+
+            // ---- Teste com Tema Claro ----
+            Console.WriteLine("\n--- Usando o Tema Claro ---");
+            gerenciador.SetTemaAtivo(temaClaro);
+
+            Botao botaoClaro = gerenciador.CriarBotao();
+            Console.Write("--> Resultado: ");
+            botaoClaro.Renderizar();
         }
     }
 }
--- Projeto1APOO/Tema.cs
+++ Projeto1APOO/Tema.cs
@@ -8,5 +8,55 @@ namespace Projeto1APOO
 {
     internal class Tema
     {
+        private string CorDeFundo { get; }
+        private string CorDaFonte { get; }
+        private string NomeDaFonte { get; }
+        private int TamanhoDaFonte { get; }
+
+        private Tema (TemaBuilder builder)
+        {
+            CorDeFundo = builder.CorDeFundo;
+            CorDaFonte = builder.CorDaFonte;
+            NomeDaFonte = builder.NomeDaFonte;
+            TamanhoDaFonte = builder.TamanhoDaFonte;
+        }
+
+        public override string ToString()
+        {
+            return $"Tema [Fundo: {CorDeFundo}, Fonte: {CorDaFonte}, Nome da Fonte: {NomeDaFonte}, Tamanho: {TamanhoDaFonte}]";
+        }
+
+        public class TemaBuilder
+        {
+            // Mesmos atributos, porém com setters públicos
+            public string CorDeFundo { get; private set; }
+            public string CorDaFonte { get; private set; }
+            public string NomeDaFonte { get; private set; }
+            public int TamanhoDaFonte { get; private set; }
+
+            public TemaBuilder ComCorDeFundo(string cor)
+            {
+                CorDeFundo = cor;
+                return this;
+            }
+
+            public TemaBuilder ComCorDaFonte(string cor)
+            {
+                CorDaFonte = cor;
+                return this;
+            }
+
+            public TemaBuilder ComFonte(string nomeFonte, int tamanhoFonte)
+            {
+                NomeDaFonte = nomeFonte;
+                TamanhoDaFonte = tamanhoFonte;
+                return this;
+            }
+
+            public Tema Build()
+            {
+                return new Tema(this);
+            }
+        }
     }
 }
